8e96f007c336841b31ec66b047ac7705896558ef
Daniel Silva <diielsilvasousa@gmail.com>
2024-12-19 23:41:19 -0300

    chore: creating shared header

---
+++ app/Views/templates/header.php
@@ -0,0 +1,16 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="<?= base_url("/categories") ?>">Categories</a></li>
+        </ul>
+        <ul>
+            <li><a href="<?= base_url("/spents") ?>">Spents</a></li>
+        </ul>
+        <ul>
+            <li><a href="<?= base_url("/reports") ?>">Report</a></li>
+        </ul>
+        <ul>
+            <li><a href="<?= base_url("/signout") ?>">Sign Out</a></li>
+        </ul>
+    </nav>
+</header>
