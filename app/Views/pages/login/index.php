7c66261a5ab73f47a296d91f13d5240ac81169f5
Daniel Silva <diielsilvasousa@gmail.com>
2024-12-21 17:47:02 -0300

    chore: adding basic css to login page

--- app/Views/pages/login/index.php
+++ app/Views/pages/login/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="<?= base_url('/assets/styles.css') ?>">
 </head>
 
 <body>
@@ -12,12 +13,17 @@
 
     </section>
 
-    <form id="login-form">
-        <input type="email" name="email" id="email" placeholder="Email" required>
-        <input type="password" name="password" id="password" placeholder="Password" minlength="6" required>
-        <button type="submit">Login</button>
-        <a href="<?= site_url('/register') ?>">Don't have an account? Register</a>
-    </form>
+    <div class="container">
+        <h1>Login</h1>
+        <form id="login-form">
+            <input type="email" name="email" id="email" placeholder="Email" required>
+            <input type="password" name="password" id="password" placeholder="Password" minlength="6" required>
+            <button type="submit" class="submit-button">Login</button>
+        </form>
+        <div>
+            <a href="<?= site_url('/register') ?>">Don't have an account? Register</a>
+        </div>
+    </div>
 
     <?php require_once(APPPATH . '/Views/pages/login/scripts.php') ?>
 
